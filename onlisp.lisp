9dac53db4ee809febf5136d8c128fb07a94c1887
arlechann <dragnov3728@gmail.com>
2023-11-03 05:27:20 +0900

    マクロを定義するマクロ

--- onlisp.lisp
+++ onlisp.lisp
@@ -846,3 +846,59 @@
           (delay-closure x)
           (funcall (delay-closure x)))
       x))
+
+;;;
+;;; マクロを定義するマクロ
+;;;
+
+;;; 省略
+
+(defmacro abbrev (short long)
+  `(defmacro ,short (&rest args)
+     `(,',long ,@args)))
+
+(defmacro abbrevs (&rest names)
+  `(progn ,@(mapcar (lambda (pair)
+                      `(abbrev ,@pair))
+                    (group names 2))))
+
+;;; 属性
+
+(defmacro propmacro (propname)
+  `(defmacro ,propname (obj)
+     `(get ,obj ',',propname)))
+
+(defmacro propmacros (&rest props)
+  `(progn (mapcar (lambda (p) `(propmacro ,p))
+                  ,props)))
+
+;;; アナフォリックマクロ
+
+(defun pop-symbol (sym)
+  (intern (subseq (symbol-name sym) 1)))
+
+(defmacro defanaph (name &optional calls (rule :all))
+  (let* ((opname (or calls (pop-symbol name)))
+         (body (case rule
+                 (:all `(anaphex1 args '(,opname)))
+                 (:first `(anaphex2 ',opname args))
+                 (:place `(anaphex3 ',opname args)))))
+    `(defmacro ,name (&rest args)
+       ,body)))
+
+(defun anaphex1 (args expr)
+  (if args
+      (let ((sym (gensym)))
+        `(let* ((,sym ,(car args))
+                (it ,sym))
+           (declare (ignorable it))
+           ,(anaphex1 (cdr args)
+                     (cons sym expr))))
+      (reverse expr)))
+
+(defun anaphex2 (op args)
+  `(let ((it ,(car args)))
+     (,op it ,@(cdr args))))
+
+(defun anaphex3 (op args)
+  `(_f (lambda (it) (,op it ,@(cdr args))) ,(car args)))
